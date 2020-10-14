dd54890e6b69e061230feb9516fe76158c22d6b5
Vlad_Nozhkin <mega.nozhkin@mail.ru>
2020-10-14 19:55:41 +0300

    added new section catalog

--- project RunSmart/src/js/script.js
+++ project RunSmart/src/js/script.js
@@ -1,35 +1,54 @@
-// $(document).ready(function() {
-//     $('.carousel__inner').slick({
-//         speed: 1200,
-//         // adaptiveHeight: true,
-//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/carousel/arrow_l.svg" alt="arrow_l"></button>',
-//         nextArrow: '<button type="button" class="slick-next"><img src="icons/carousel/arrow_r.svg" alt="arrow_r"></button>',
-//         responsive: [
-//             {
-//               breakpoint: 991,
-//               settings: {
-//                 dots: true,
-//                 arrows: false,
-//               }
-//             }
-//         ]
-//     });
-//   });
+$(document).ready(function() {
+    $('.carousel__inner').slick({
+        speed: 1200,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/carousel/arrow_l.svg" alt="arrow_l"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/carousel/arrow_r.svg" alt="arrow_r"></button>',
+        responsive: [
+            {
+              breakpoint: 991,
+              settings: {
+                dots: true,
+                arrows: false,
+              }
+            }
+        ]
+    });
 
-const slider = tns({
-    container: '.carousel__inner',
-    speed: 1200,
-    items: 1,
-    slideBy: 'page',
-    autoplay: false,
-    nav: false,
-    controls: false
-});
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+        $(this)
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
 
-document.querySelector('.prev').addEventListener('click', function () {
-    slider.goTo('prev');
-});
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            });
+        });
+    }
 
-document.querySelector('.next').addEventListener('click', function () {
-    slider.goTo('next');
-});
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+  });
+
+// const slider = tns({
+//     container: '.carousel__inner',
+//     speed: 1200,
+//     items: 1,
+//     slideBy: 'page',
+//     autoplay: false,
+//     nav: false,
+//     controls: false
+// });
+
+// document.querySelector('.prev').addEventListener('click', function () {
+//     slider.goTo('prev');
+// });
+
+// document.querySelector('.next').addEventListener('click', function () {
+//     slider.goTo('next');
+// });
